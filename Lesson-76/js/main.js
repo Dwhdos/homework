a4480c6b55a91f0e47aac337225ff234151f62e9
Dwhdos <167628831+Dwhdos@users.noreply.github.com>
2024-10-19 19:12:18 +0500

    Add files via upload

---
+++ Lesson-76/js/main.js
@@ -0,0 +1,11 @@
+const firstName = 'Dima';
+const lastName = 'Lyamin';
+const age = '16';
+const city = 'Ekb';
+const eyeСolor = 'evegreen swamp';
+
+console.log('const и log различаются тем, что к первому нельзя присвоить другое значение, а ко второму можно');
+console.log(firstName, lastName);
+console.log('age -', age, 'yo');
+console.log('my city -', city);
+console.log('eye color -', eyeСolor);
